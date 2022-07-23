f5c9b8f77b038bd7333f9890feb2c24dc9eeef4f
AriefLinux <79347655+Ariefna@users.noreply.github.com>
2022-07-23 20:05:53 +0700

    make it better

--- app.js
+++ app.js
@@ -22,17 +22,7 @@ const client = new Client({
   restartOnAuthFail: true,
   puppeteer: {
     headless: true,
-    args: [
-      "--no-sandbox",
-      "--unhandled-rejections=strict",
-      "--disable-setuid-sandbox",
-      "--disable-dev-shm-usage",
-      "--disable-accelerated-2d-canvas",
-      "--no-first-run",
-      "--no-zygote",
-      "--single-process", // <- this one doesn't works in Windows
-      "--disable-gpu",
-    ],
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
   },
   authStrategy: new LocalAuth({ clientId: "client-one" }),
 });
